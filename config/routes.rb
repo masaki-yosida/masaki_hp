16c14c42cc2605d56d7bb330f26b1f434fb6e089
masaki-yosida <yashukasono@gmail.com>
2024-01-20 23:35:07 +0900

    indexまで表示可能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :pages
+  root 'pages#index'
 end
+
